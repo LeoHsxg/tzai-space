2fe478e4a576a47e4e03cb0a91dc6fd6c1ef5d7c
LeoHsxg <leosimba9487@gmail.com>
2024-09-19 17:54:34 +0800

    init 2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Calendar from "./Components/Calendar";
+import "./Components/Calendar.css";
+
+const App = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
+
