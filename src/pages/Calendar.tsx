861368a7940f52abf2e092e8ee447ac9c3e17afd
LeoHsxg <leosimba9487@gmail.com>
2025-04-01 16:02:38 +0800

    style:  為事件新增分隔線
    調整項目:
     為日曆加載出事件以後，在昨日與今日的事件新增分隔線，方便辨認天數，因應仁齋住戶普遍活躍時間為 12 點，因此此項功能有其必要

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -11,7 +11,10 @@ const Calendar: React.FC = () => {
   const [value, setValue] = React.useState<Dayjs | null>(dayjs()); // 當前選中的日期
   const [events, setEvents] = useState<Event[]>([]); // 儲存事件資料
   const [filteredEvents, setFilteredEvents] = useState<Event[]>([]); // 當天的事件
-  const [filteredAmount, setFilteredAmount] = useState<number>(0);
+  const [filteredAmount, setFilteredAmount] = useState<number>(0); // 當天的事件數量
+  const [spanningEvents, setSpanningEvents] = useState<Event[]>([]); // 跨天的事件
+  const [todayEvents, setTodayEvents] = useState<Event[]>([]); // 今天的事件
+
   const [selectedEvent, setSelectedEvent] = useState<Event | null>(null); // 選中的事件
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState<boolean>(false); // 新增 loading 狀態
@@ -65,8 +68,13 @@ const Calendar: React.FC = () => {
       const ed = dayjs(event.end.dateTime).format("YYYY-MM-DD");
       return sd <= selectedDate && selectedDate <= ed;
     });
+    // 拆成兩組
+    const spanningEventsData = dailyEvents.filter(event => dayjs(event.start.dateTime).isBefore(selectedDate));
+    const todayEventsData = dailyEvents.filter(event => dayjs(event.start.dateTime).isSame(selectedDate, "day"));
     setFilteredEvents(dailyEvents);
     setFilteredAmount(dailyEvents.length);
+    setSpanningEvents(spanningEventsData);
+    setTodayEvents(todayEventsData);
   }, [value, events]); // 當 `value` 或 `events` 改變時重新篩選
 
   return (
@@ -91,9 +99,21 @@ const Calendar: React.FC = () => {
           </div>
         ) : (
           <div className="self-stretch py-2.5 flex-col justify-start items-center gap-3.5 flex">
-            {filteredEvents.map((event, index) => (
+            {spanningEvents.map((event, index) => (
               <Reserve onClick={() => handleClickOpen(event)} key={index} event={event} />
             ))}
+
+            {/* 如果有橫跨事件又有今天事件，插入分隔線 */}
+            {spanningEvents.length > 0 && todayEvents.length > 0 && (
+              <div className="w-full px-2">
+                <div className="w-full border-t-2 border-dashed border-gray-400/40"></div>
+              </div>
+            )}
+
+            {todayEvents.map((event, index) => (
+              <Reserve onClick={() => handleClickOpen(event)} key={spanningEvents.length + index} event={event} />
+            ))}
+
             <MyDialog open={open} onClose={handleClose} event={selectedEvent || ({} as Event)} />
           </div>
         )}
